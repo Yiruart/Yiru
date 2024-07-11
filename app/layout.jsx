08204c4b04db58d583ec10ef7fc464c8c226a506
norafudev <blinkbyhayley@gmail.com>
2024-07-11 14:08:31 +0900

    css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,36 @@
 import { Layout } from "@/components/dom/Layout";
 import "@/global.css";
+import localFont from "next/font/local";
 
 export const metadata = {
   title: "Yiru Wang's Portfolio",
   description: "An artist based in London.",
 };
 
+const Proxima = localFont({
+  src: [
+    {
+      path: "../public/fonts/proximanova_light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/proximanova_regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/proximanova_bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="antialiased">
-      <body>
+      <body className={Proxima.className}>
         <Layout>{children}</Layout>
       </body>
     </html>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 w-screen flex justify-between items-center gap-x-1 p-2 pr-6 z-50 text-xs sm:text-base h-8">
+    <header className="fixed top-0 w-screen flex justify-between items-center gap-x-1 p-2 sm:pr-6 z-50 text-xs sm:text-base h-8">
       <div>
         YIRU·W
         <span className="pl-1">®</span>
