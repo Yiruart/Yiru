f186a875686ecbaaf39be3e9134ee777215c9007
norafudev <blinkbyhayley@gmail.com>
2024-07-13 00:18:33 +0900

    fix bug

--- src/components/menu/projects/descriptions/index.jsx
+++ src/components/menu/projects/descriptions/index.jsx
@@ -9,21 +9,22 @@ export default function index({ data, selectedProject }) {
   return (
     <div className={styles.descriptions}>
       {data.map((project, i) => {
-        const { title, description } = project;
+        const { title, description, href } = project;
         return (
-          <div
+          <a
             key={i}
             className={styles.description}
             style={{
               clipPath:
                 selectedProject == i ? "inset(0 0 0)" : "inset(50% 0 50%",
             }}
+            href={href}
           >
             <p>{crop(title, 9)}</p>
             <p className="text-black text-xs sm:text-lg line-clamp-1 sm:line-clamp-2">
               {description}
             </p>
-          </div>
+          </a>
         );
       })}
     </div>
