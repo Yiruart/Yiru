0bd0a2e4499021be7a6f008ed71aab016ecf8a6f
norafudev <blinkbyhayley@gmail.com>
2024-07-14 20:27:01 +0900

    css

--- src/components/who i am/WhoIAm.jsx
+++ src/components/who i am/WhoIAm.jsx
@@ -7,7 +7,7 @@ const WhoIAm = () => {
       className="relative h-auto w-screen flex flex-col p-8 sm:p-16"
       id={data.slug}
     >
-      <h1 className="flex justify-center font-light text-4xl sm:text-6xl mb-4 sm:mb-8 z-10">
+      <h1 className="flex justify-center font-light text-4xl sm:text-6xl mb-8 z-10">
         {data.title}
       </h1>
       <p>{data.description}</p>
