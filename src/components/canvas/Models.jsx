ccf37c24b456383396a67e40fbbdb7c910bac4a8
Yiruart <yiru.wang.art@gmail.com>
2024-07-21 08:53:01 +0900

    Update Models.jsx

--- src/components/canvas/Models.jsx
+++ src/components/canvas/Models.jsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Gltf, Environment, PerspectiveCamera } from "@react-three/drei";
+import { Gltf, PerspectiveCamera } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useRef, useState, Suspense } from "react";
-import { useCursor, MeshDistortMaterial } from "@react-three/drei";
-import { useRouter } from "next/navigation";
+import { useRef, Suspense } from "react";
+
 import { easing } from "maath";
 
 // chaos
@@ -65,33 +64,9 @@ export const Chaos = ({ ...props }) => {
   );
 };
 
-export const ChaosForPhone = ({ ...props }) => {
-  const group = useRef();
-  const light = useRef();
-
-  useFrame((state, delta) => {
-    easing.dampE(
-      group.current.rotation,
-      [0, -state.pointer.x * (Math.PI / 10), 0],
-      1.5,
-      delta
-    );
-    easing.damp3(
-      group.current.position,
-      [0, 0, 1 - Math.abs(state.pointer.x)],
-      1,
-      delta
-    );
-    easing.damp3(
-      light.current.position,
-      [state.pointer.x * 8, 0, 8 + state.pointer.y * 2],
-      0.2,
-      delta
-    );
-  });
-
+export const ChaosForPhone = () => {
   return (
-    <group ref={group} {...props}>
+    <group>
       <Gltf
         src="/models/chaos/1c_env.glb"
         position={[-4, -2.7, -1]}
@@ -107,31 +82,22 @@ export const ChaosForPhone = ({ ...props }) => {
       <spotLight
         angle={0.5}
         penumbra={0.5}
-        ref={light}
         castShadow
         intensity={15}
         shadow-mapSize={1024}
         shadow-bias={-0.001}
+        position={[6, 0, 6]}
+        // ref={light}
       >
         <orthographicCamera
           attach="shadow-camera"
           args={[-10, 10, -10, 10, 0.1, 50]}
         />
+        <pointLight position={[20, 30, 10]} intensity={0.1} decay={0.2} />
+        <pointLight position={[-10, 10, -10]} decay={0.9} />
       </spotLight>
     </group>
   );
-};
-
-export const ModelForPhone = () => {
-  <group>
-    <Environment preset="sunset" />
-    <Gltf
-      src="/models/chaos/1c.glb"
-      position={[0, 1, 0]}
-      scale={[0.1, 0.1, 0.1]}
-      rotation={[0, Math.PI * 0.78, 0]}
-    />
-  </group>;
 };
 
 export function Light() {
